4ae35ff5782ed0934ac2ef8d063ad4ac15462537
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-22 18:53:50 +0530

    added : throwing custom exception incase of invalid user details

---
+++ main/java/com/bridgelabz/userregistration_junit/ValidationException.java
@@ -0,0 +1,16 @@
+/************************************************************
+ * @author Rosy Rupali
+ * @Version 1.0
+ * @since 21-06-2021
+ *
+ ************************************************************/
+package com.bridgelabz.userregistration_junit;
+
+public class ValidationException extends Exception {
+
+	public ValidationException(String message) {
+		super(message);
+	}
+	
+
+}
